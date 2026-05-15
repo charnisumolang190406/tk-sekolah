cd98c7ad88d5c753d30f1d548ee49cd446db5829
charnisumolang190406 <charnisumolang98@gmail.com>
2026-05-15 23:30:07 +0800

    manual cloudinary upload

--- backend/src/controllers/galeri.controller.js
+++ backend/src/controllers/galeri.controller.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const cloudinary = require("../config/cloudinary");
 
-// GET ALL
+// GET
 exports.getGaleri = async (req, res) => {
   try {
     const data = await prisma.galeri.findMany();
@@ -21,42 +21,9 @@ exports.createGaleri = async (req, res) => {
       return res.status(400).json({ message: "File tidak ada" });
     }
 
-    const result = await uploadFromBuffer(req.file.buffer);
-
-    const data = await prisma.galeri.create({
-      data: {
-        judul: req.body.judul,
-        foto: result.secure_url,
-      },
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "tk-sekolah-galeri",
     });
-
-    res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err.message });
-  }
-};
-
-const fs = require("fs");
-
-const stream = require("stream");
-
-const uploadFromBuffer = (fileBuffer) => {
-  return new Promise((resolve, reject) => {
-    const passthrough = new stream.PassThrough();
-    passthrough.end(fileBuffer);
-
-    const cloudStream = cloudinary.uploader.upload_stream(
-      { folder: "tk-sekolah-galeri" },
-      (err, result) => {
-        if (err) reject(err);
-        else resolve(result);
-      }
-    );
-
-    passthrough.pipe(cloudStream);
-  });
-};
 
     const data = await prisma.galeri.create({
       data: {
