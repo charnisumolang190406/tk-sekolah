24d58dacad624e657f3bc0779c44db138d2aa782
charnisumolang190406 <charnisumolang98@gmail.com>
2026-05-15 21:58:44 +0800

    fix ci

--- backend/src/middleware/upload.js
+++ backend/src/middleware/upload.js
@@ -3,19 +3,17 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
 const storage = new CloudinaryStorage({
-  cloudinary,
-  params: async (req, file) => ({
-    folder: "tk-sekolah-galeri",
-    format: "jpg",
-    resource_type: "image",
-  }),
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: "tk-sekolah-galeri",
+      resource_type: "auto",
+    };
+  },
 });
 
 const upload = multer({
   storage,
-  limits: {
-    fileSize: 2 * 1024 * 1024,
-  },
 });
 
 module.exports = upload;
