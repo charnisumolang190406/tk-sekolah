63305fef781edd16a05a70f4e2106a4bd36954d4
charnisumolang190406 <charnisumolang98@gmail.com>
2026-05-18 11:08:59 +0800

    manual cloudinary upload

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,19 +1,36 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    return res.status(401).json({ message: "Token tidak ditemukan" });
-  }
-
-  const token = authHeader.split(" ")[1];
-
   try {
+    console.log("➡️ AUTH MIDDLEWARE");
+
+    const authHeader = req.headers.authorization;
+
+    console.log("HEADER:", authHeader);
+
+    if (!authHeader) {
+      return res.status(401).json({
+        message: "Token tidak ditemukan",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    console.log("TOKEN:", token);
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("✅ TOKEN VALID");
+
     req.admin = decoded;
+
     next();
+
   } catch (err) {
-    return res.status(403).json({ message: "Token tidak valid" });
+    console.log("❌ AUTH ERROR:", err.message);
+
+    return res.status(403).json({
+      message: "Token tidak valid",
+    });
   }
 };
