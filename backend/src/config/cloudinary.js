06ac776cffb74f321db776a2576ddb8351b3d905
charnisumolang190406 <charnisumolang98@gmail.com>
2026-05-19 09:59:26 +0800

    j

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -4,17 +4,15 @@ require("dotenv").config({
 
 const cloudinary = require("cloudinary").v2;
 
-console.log("CLOUD TEST:", {
-  name: process.env.CLOUD_NAME,
-  key: process.env.CLOUD_API_KEY,
-  secret: process.env.CLOUD_API_SECRET ? "ADA" : "KOSONG",
+// Cloudinary otomatis baca CLOUDINARY_URL
+cloudinary.config({
+  cloudinary_url: process.env.CLOUDINARY_URL
 });
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-  secure: true,
+console.log("CLOUD TEST:", {
+  name: cloudinary.config().cloud_name,
+  key: cloudinary.config().api_key,
+  secret: cloudinary.config().api_secret ? "ADA" : "KOSONG",
 });
 
 module.exports = cloudinary;
